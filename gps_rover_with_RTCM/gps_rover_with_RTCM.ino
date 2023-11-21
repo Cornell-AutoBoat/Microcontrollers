434027f3ce223a7c0a7e24005b6bfd66c460ad89
Eric <ewz4@cornell.edu>
2023-11-21 14:10:39 -0500

    Added rover directory and todos

---
+++ gps_rover_with_RTCM/gps_rover_with_RTCM.ino
@@ -0,0 +1,4 @@
+// Put your code here
+// Base it off of the ZED Example 15 and add the following:
+// > Serial read the RTCM data
+// > Add position polling before and after pushing RTCM to the GNSS module, and a delay after a uncorrected and corrected print cycle
